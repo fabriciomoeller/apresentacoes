2463a130f24d7d1212e9b0a5a9760ccace49ae8b
fabriciomoeller <fabriciomoeller@gmail.com>
2026-04-22 16:11:45 -0300

    deploy: atualiza apresentações Slidev

---
+++ compras/assets/slidev/default-DaFvkbOg.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-8v_DDP7h.js";import{_ as r}from"../index-DedZWH0n.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
